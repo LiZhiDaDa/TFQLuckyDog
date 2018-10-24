7ded97c13191300a4abfa67643c049853a29d9f0
王立志 <1226169349@qq.com>
2018-10-24 18:30:31 +0800

    code initial

---
+++ TFQLuckyDog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TFQLuckyDog
+//
+//  Created by 王立志 on 2018/10/22.
+//  Copyright © 2018年 Thread_Fight_Queue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TFQLuckyDog/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  TFQLuckyDog
+//
+//  Created by 王立志 on 2018/10/22.
+//  Copyright © 2018年 Thread_Fight_Queue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSArray *nameArray = @[@"三余uncle", @"_啟灮丶", @"张乾TH"];
+    NSLog(@"%@", nameArray);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
